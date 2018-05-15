cb43347664920cdf44d306dd5079d74d684bb134
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-15 15:12:40 -0300

    basic list working

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app style="max-height: 100%">
     <v-toolbar
       app
       :clipped-left="clipped"
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>menu</v-icon>
       </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
     </v-toolbar>
+
     <v-content>
       <router-view/>
     </v-content>
+
     <v-navigation-drawer
       temporary
       :right="right"
@@ -64,7 +31,9 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+      <v-layout column align-center>
+        <span>&copy; 2017 Jacob Smith</span>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
@@ -81,11 +50,16 @@ export default {
         title: 'Inspire'
       }],
       miniVariant: false,
-      right: true,
+      right: false,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Curriculum Mapper'
     }
   },
   name: 'App'
 }
 </script>
+
+<style>
+  html, body {margin:0;padding:0;height:100%;}
+  {box-sizing:border-box;}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-container fluid>
+    <v-layout
+      column
+      v-scroll:#scroll-target="onScroll"
+    >
+      <v-list
+        two-line
+        id="scroll-target"
+        style="max-height: 100%"
+        class="scroll-y">
+
+        <template v-for="(item, index) in items">
+          <v-list-tile
+            :key="item.title"
+            avatar
+            ripple
+            @click="open(index)"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+        </template>
+      </v-list>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          action: '15 min',
+          headline: 'Dr. MacIsaac',
+          title: 'CS1013',
+          subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+        },
+        {
+          action: '2 hr',
+          headline: 'Dr. MacIsaac',
+          title: 'CS1073',
+          subtitle: "Wish I could come, but I'm out of town this weekend."
+        },
+        {
+          action: '6 hr',
+          headline: 'Dr. MacIsaac',
+          title: 'CS1083',
+          subtitle: 'Do you have Paris recommendations? Have you ever been?'
+        },
+        {
+          action: '12 hr',
+          headline: 'Dr. MacIsaac',
+          title: 'CS2043',
+          subtitle: 'Have any ideas about what we should get Heidi for her birthday?'
+        },
+        {
+          action: '18hr',
+          headline: 'Dr. MacIsaac',
+          title: 'CS1303',
+          subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.'
+        }
+      ],
+      offsetTop: 0
+    }
+  },
+  methods: {
+    open (index) {
+      console.log('Opening ' + index)
+    },
+    onScroll (e) {
+      this.offsetTop = e.target.scrollTop
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#list {
+  max-width: 350px;
+}
+</style>
